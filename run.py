be4f3ab0a2121bf711edc7c9723fd5c9877580fd
Lorraine Scanlon <lorrainescanlon@gmail.com>
2024-10-21 12:20:48 +0000

    functions to retrieve username from user and validate input, return error if the username exceeds length

--- run.py
+++ run.py
@@ -15,4 +15,39 @@ SHEET = GSPREAD_CLIENT.open('edsongs')
 songs = SHEET.worksheet('songs')
 
 data = songs.get_all_values()
-print(data)
+
+
+def get_username():
+    """
+    Get username input from user
+    """
+
+    while True:
+        print("Welcome to Scramble Ed\n")
+        print("Can you guess the Ed Sheeran song title before Ed tunes his guitar?\n")
+        print("You have 3 attempts to unscramble the song title\n")
+        print("Enter a username to start the game, max length 12 characters\n")
+
+        username = input("Username here:\n")
+
+        if validate_username(username):
+            print("Username is valid")
+            break
+    return username
+
+def validate_username(name):
+    """
+    Checks that the username entered does not exceed 12 characters
+    """
+    try:
+        if len(name) > 12:
+            raise ValueError(
+                "Username exceeds length"
+            )
+
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+    return True
+
+get_username()
